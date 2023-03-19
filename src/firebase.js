a2d571ae5d77ed1bcc68e423c47f1cd5608c9095
Nikola <rodnik357@gmail.com>
2023-03-19 20:46:27 +0100

    sedam velicanstvenih

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4Prpp58l0-GbMl5JmBKMj-ntjSqsHk98",
-  authDomain: "trachat-5777c.firebaseapp.com",
-  projectId: "trachat-5777c",
-  storageBucket: "trachat-5777c.appspot.com",
-  messagingSenderId: "974904476470",
-  appId: "1:974904476470:web:2b64e98d65b420256f2997",
-  measurementId: "G-3GG11GKDKP",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
